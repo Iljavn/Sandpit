d9efa8677d1f7054d378246477be88a92c3c5a3f
jeaninetopelen <49662010+jeaninetopelen@users.noreply.github.com>
2019-04-16 15:10:28 +1200

    Update FirstTry.py
    added some lines

--- FirstTry.py
+++ FirstTry.py
@@ -5,10 +5,12 @@ Created on Tue Apr 16 14:14:52 2019
 @author: IljaVN
 """
 
-#woohoo first python file 
+#woohoo first python file
 x=2
 print (x)
 x=x+5
 print(x)
 x=x*3
 print(x)
+a = 40
+print(a-x)
